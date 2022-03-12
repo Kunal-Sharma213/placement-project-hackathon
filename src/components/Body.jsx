d0dceef0a1e2b4513c0133674e8a74479d7bd27f
Kunal Sharma <Kvsharma2003@gmail.com>
2022-03-12 23:07:33 +0530

    add header

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Body = () => {
+	return (
+		<body>
+			<img src="../../public/mt1.jpg" alt="" />
+		</body>
+	);
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,47 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-  return (
-    <div className='base'>
-      <div>
-        WEBSITE NAME
-      </div>
+	return (
+		<header className="header">
+			<div class="website-name">WEBSITE NAME</div>
+			<nav className="main-nav">
+				<ul className="main-nav-list">
+					<li>
+						<Link to="/about" className="main-nav-link">
+							<p>About Us</p>
+						</Link>
+					</li>
+					<li>
+						<Link to="/company" className="main-nav-link">
+							<p>Company</p>
+						</Link>
+					</li>
+					<li>
+						<Link to="/education" className="main-nav-link">
+							<p>Education</p>
+						</Link>
+					</li>
+					<li>
+						<Link to="/internships" className="main-nav-link">
+							<p>Internships</p>
+						</Link>
+					</li>
+					<li>
+						<Link to="/vacancies" className="main-nav-link">
+							<p>Current Vacancies</p>
+						</Link>
+					</li>
+					<li>
+						<Link to="/" className="btn">
+							<p>Join Us</p>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
-      <div>
-        <Link to="/about"><p>About Us</p></Link>
-        <Link to="/company"><p>Company</p></Link>
-        <Link to="/education"><p>Education</p></Link>
-        <Link to="/internships"><p>Internships</p></Link>
-        <Link to="/vacancies"><p>Current Vacancies</p></Link>
-      </div>
-
-      <div>
-        <Button >Join Us</Button>
-      </div>
-    </div>
-  )
-}
-
-export default Header
+export default Header;
